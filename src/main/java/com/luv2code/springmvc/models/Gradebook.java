d54ea30da6359f45f5c663ad5703bebc1aa50229
Vikam <deokarvikram57@gmail.com>
2022-07-11 16:14:27 -0500

    Initial commit

---
+++ src/main/java/com/luv2code/springmvc/models/Gradebook.java
@@ -0,0 +1,29 @@
+package com.luv2code.springmvc.models;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Gradebook {
+
+    List<GradebookCollegeStudent> students = new ArrayList<>();
+
+    public Gradebook() {
+      
+    }
+    public Gradebook(List<GradebookCollegeStudent> students) {
+        this.students = students;
+    }
+
+    public List<GradebookCollegeStudent> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<GradebookCollegeStudent> students) {
+        this.students = students;
+    }
+}
